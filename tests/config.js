833413e6b4db392a19846b30ffacf9623dc8e813
Viljami Peltola <viljami.peltola@iki.fi>
2015-05-28 09:33:50 +1000

    Testing tools - make into a dependency.

---
+++ tests/config.js
@@ -0,0 +1,33 @@
+(function(global) {
+    global.monkeytestjs = {
+        "facebookId": "000000000000000",
+
+        "local": {
+            "env": ['localhost']
+        },
+        "stage": {
+            "env": [/STAGE URL OR PART OF/],
+            "facebookId": "222222222222222"
+        },
+        "beta": {
+            "env": [/BETA URL OR PART OF/],
+            "facebookId": "33333333333333333"
+        },
+        "production": {
+            "env": [/PRODUCTION URL OR PART OF/],
+            "facebookId": "4444444444444444444"
+        },
+        "testsDir": "mytests",
+        "globalTests": [
+            "global/not_server_error.js",
+            "global/is_html_w3c_valid.js",
+            "global/has_utf8_metatag.js",
+            "global/has_google_analytics.js"
+        ],
+        "pages": [{
+            "url": "../index.html"
+        }],
+        "proxyUrl": "core/proxy.php?mode=native&url=<%= url %>",
+        "loadSources": true
+    };
+})(this);
